012386c5547dcdba53d5fa05d2a841abd0519cc2
gregoryamitten@gmail.com <gregoryamitten@gmail.com>
2019-03-01 17:20:49 +0000

    Fully implemented simulation framework

--- sim/example.py
+++ sim/example.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 import assignment
 import enum
@@ -42,7 +43,7 @@ def findLight(sensors):
     global moveCount, rotateCount, rotateToCorrectAngleCount, highestReadForRotation, rotationAtHighestRead, hitLight, movesToLight
 
     if moveCount < 100:
-        if(sensors[0] > 15):
+        if(sensors[0] > 1):
             hitLight = True
         # print("moving")
         moveCount += 1
@@ -87,16 +88,44 @@ def getHome(sensors):
 
     return [0,0], None
 
-i = 0
+def runSimulations(count):
+    task1Cumulative = 0
+    task2Cumulative = 0
 
+    successfulTask1Count = 0.0
+    successfulTask2Count = 0.0
 
+    for i in range(1, count + 1):
+        w = assignment.World()
+        poses, sensations, actions, states = w.simulate(constantController)
 
-for i in range(0, 2000):
-    w = assignment.World()
-    poses, sensations, actions, states = w.simulate(constantController)
+        reset()
 
-    reset()
+        t1f = w.task1fitness(poses)
+        t2f = w.task2fitness(poses) - t1f
 
-    print("Fitness on task 1: %f" % w.task1fitness(poses))
-    print("Fitness on task 2: %f" % w.task2fitness(poses))
-    print("=" * 33)
+        if not t1f == np.inf:
+            successfulTask1Count += 1
+            task1Cumulative += t1f
+
+        if not t2f == np.inf:
+            successfulTask2Count += 1
+            task2Cumulative += t2f
+
+        print("Simulation - %d |" % i)
+        print("-----------------")
+        print("Fitness on task 1: %f" % w.task1fitness(poses))
+        print("Fitness on task 2: %f" % w.task2fitness(poses))
+        print("=" * 33)
+
+    if successfulTask1Count > 0: print("Task 1 average fitness: %f" % (task1Cumulative / successfulTask1Count))
+    else: print("Task 1 succeeded 0 times")
+
+    print("Task 1 Success rate: %f%%" % ((successfulTask1Count / count * 1.0) * 100) if successfulTask1Count > 0 else '0%')
+
+    if successfulTask2Count > 0: print("Task 2 average fitness: %f" % (task2Cumulative / successfulTask2Count))
+    else: print("Task 2 succeeded 0 times")
+
+    print("Task 2 Success rate: %f%%" % ((successfulTask2Count/ count * 1.0) * 100) if successfulTask2Count > 0 else '')
+
+runSimulations(200)
